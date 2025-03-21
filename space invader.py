1360bfdcadfc275ba10f015ecc92c91994c4ae47
srikar31312 <vadlurisrikar@gmail.com>
2025-03-21 22:50:20 +0530

    Space invader

---
+++ space invader.py
@@ -0,0 +1,51 @@
+import pygame
+import math
+import random
+from pygame import mixer
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 500
+PLAYER_START_X = 370
+PLAYER_START_Y = 380
+ENEMY_START_Y_MIN = 50
+ENEMY_START_Y_MAX = 150
+ENEMY_SPEED_X = 1
+ENEMY_SPEED_Y = 20
+BULLET_SPEED_Y = 10
+COLLISION_DISTANCE = 27
+
+# INITIALIZE PYGAME
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# BACKGROUND
+background = pygame.image.load('background.png')
+
+# Caption and Icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# player
+playerImg = pygame.image.load('player.png')
+playerX = PLAYER_START_X
+playery = PLAYER_START_Y
+playerX_change = 0
+
+# Enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 64)) #64 is the size of the enemy
+    enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
+    enemyX_change.append(ENEMY_SPEED_X)
+    enemyY_change.append(ENEMY_SPEED_Y)
